e8fdb781afe005d138fe3c09d75f5a4786925bc9
innovativeroh <innovativeroh@gmail.com>
2025-09-19 08:13:16 +0530

    Next.TS Configs Changes Done To Ignore Unwanted Issues In Production

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbo: {
+      resolveAlias: {},
+    },
+  },
+  webpack(config, { isServer }) {
+    if (process.env.NODE_ENV === "production") {
+      return config; // stick with Webpack
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
