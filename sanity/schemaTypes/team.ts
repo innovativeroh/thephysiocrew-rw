f346ee93ec8c6d617b643b0fd4b52ec3c42dff3e
innovativeroh <innovativeroh@gmail.com>
2025-09-19 10:19:23 +0530

    Team Base Have Been Initiated To

---
+++ sanity/schemaTypes/team.ts
@@ -0,0 +1,34 @@
+// /sanity/schemaTypes/team.ts
+import { defineField, defineType } from "sanity";
+
+export const team = defineType({
+  name: "team",
+  title: "Team",
+  type: "document",
+  fields: [
+    defineField({
+      name: "name",
+      title: "Name",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "designation",
+      title: "Designation",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "text",
+      rows: 3,
+    }),
+    defineField({
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: { hotspot: true },
+    }),
+  ],
+});
