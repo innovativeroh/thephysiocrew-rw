e64a350423966362f3bb7276d1e7ddad16628063
innovativeroh <innovativeroh@gmail.com>
2025-09-19 08:41:06 +0530

    Added Blogs Schema

---
+++ sanity/schemaTypes/blog.ts
@@ -0,0 +1,54 @@
+// /sanity/schemaTypes/blog.ts
+import { defineField, defineType } from "sanity";
+
+export const blog = defineType({
+  name: "blog",
+  title: "Blog",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (Rule) => Rule.required().min(5).max(80),
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "author",
+      title: "Author",
+      type: "string",
+    }),
+    defineField({
+      name: "publishedAt",
+      title: "Published At",
+      type: "datetime",
+    }),
+    defineField({
+      name: "excerpt",
+      title: "Excerpt",
+      type: "text",
+      rows: 3,
+    }),
+    defineField({
+      name: "mainImage",
+      title: "Main Image",
+      type: "image",
+      options: { hotspot: true },
+    }),
+    defineField({
+      name: "content",
+      title: "Content",
+      type: "array",
+      of: [{ type: "block" }, { type: "image" }],
+    }),
+  ],
+});
